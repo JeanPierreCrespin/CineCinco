c11b616233e7df0f7c9c26455aed52adcc17a93e
octaviofarinia <octaviofarinia@gmail.com>
2021-04-05 16:52:30 -0300

    primer commit

---
+++ src/main/java/com/QuintoTrainee/CineCinco/CineCincoApplication.java
@@ -0,0 +1,13 @@
+package com.QuintoTrainee.CineCinco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineCincoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineCincoApplication.class, args);
+	}
+
+}
