5d53e71fd16c4ab7eda4b4ecd57297c6fc23f4ab
Octavio Farinia <octaviofarinia@gmail.com>
2021-04-21 20:27:12 -0300

    implementados metodos basicos de funcion y compra service

--- src/main/java/com/QuintoTrainee/CineCinco/services/CompraService.java
+++ src/main/java/com/QuintoTrainee/CineCinco/services/CompraService.java
@@ -1,7 +1,16 @@
 package com.QuintoTrainee.CineCinco.services;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.QuintoTrainee.CineCinco.converters.CompraConverter;
+import com.QuintoTrainee.CineCinco.entities.Compra;
+import com.QuintoTrainee.CineCinco.enums.Estado;
+import com.QuintoTrainee.CineCinco.exceptions.WebException;
+import com.QuintoTrainee.CineCinco.models.CompraModel;
+import com.QuintoTrainee.CineCinco.repositories.CompraRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -9,4 +18,47 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class CompraService {
 
+	@Autowired
+	private CompraConverter compraConverter;
+	@Autowired
+	private CompraRepository compraRepository;
+	
+	public void validar(CompraModel compraModel) throws WebException {
+		
+		try {
+			Enum.valueOf(Estado.class, compraModel.getEstado().toString());
+		} catch (IllegalArgumentException e) {
+			throw new WebException("El estado indicado no es un estado valido");
+		} catch (NullPointerException e) {
+			throw new WebException("La compra debe tener un estado");
+		}
+	}
+	
+	public Compra guardar(CompraModel compraModel) throws WebException {
+		
+		validar(compraModel);
+		
+		Compra compraEntity = compraConverter.modelToEntity(compraModel);
+		
+		if (compraEntity.getAlta() != null) {
+			compraEntity.setModificacion(new Date());
+		} else {
+			compraEntity.setAlta(new Date());
+		}
+		
+		return compraRepository.save(compraEntity);
+		
+	}
+	
+	public void hardDelete(CompraModel compraModel) throws WebException {
+		Compra compraEntity = compraConverter.modelToEntity(compraModel);
+		compraRepository.delete(compraEntity);
+	}
+	
+	public Compra softDelete(CompraModel compraModel) throws WebException {
+		Compra compraEntity = compraConverter.modelToEntity(compraModel);
+		compraEntity.setBaja(new Date());
+		return compraRepository.save(compraEntity);
+	}
+	
 }
--- src/main/java/com/QuintoTrainee/CineCinco/services/FuncionService.java
+++ src/main/java/com/QuintoTrainee/CineCinco/services/FuncionService.java
@@ -1,7 +1,16 @@
 package com.QuintoTrainee.CineCinco.services;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.QuintoTrainee.CineCinco.converters.FuncionConverter;
+import com.QuintoTrainee.CineCinco.entities.Funcion;
+import com.QuintoTrainee.CineCinco.enums.Idioma;
+import com.QuintoTrainee.CineCinco.exceptions.WebException;
+import com.QuintoTrainee.CineCinco.models.FuncionModel;
+import com.QuintoTrainee.CineCinco.repositories.FuncionRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -9,4 +18,68 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class FuncionService {
 
+	@Autowired
+	private FuncionRepository funcionRepository;
+	@Autowired
+	private FuncionConverter funcionConverter;
+	
+	public void validar(FuncionModel funcionModel) throws WebException {
+		
+		if (funcionModel.getFecha() == null) {
+			throw new WebException("La funcion debe contar con una fecha");
+		}
+		
+		if (funcionModel.getHorario() == null) {
+			throw new WebException("La funcion debe contar con un horario");
+		}
+		
+		if (funcionModel.getPrecioEntrada() < 0) {
+			throw new WebException("El precio de entrada indicado para la funcion es invalido");
+		}
+		
+		try {
+			Enum.valueOf(Idioma.class, funcionModel.getIdioma().toString());
+		} catch (IllegalArgumentException e) {
+			throw new WebException("El idioma indicado no es un idioma valido");
+		} catch (NullPointerException e) {
+			throw new WebException("La funcion debe contener un idioma");
+		}
+		
+	}
+	
+	//persiste el modelo
+	public Funcion guardar(FuncionModel funcionModel) {
+		
+		try {
+			
+			validar(funcionModel);
+			
+			Funcion funcionEntity = funcionConverter.modelToEntity(funcionModel);
+			
+			if (funcionEntity.getAlta() != null) {
+				funcionEntity.setModificacion(new Date());
+			} else {
+				funcionEntity.setAlta(new Date());
+			}
+			
+			return funcionRepository.save(funcionEntity);
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		return null;
+	}
+	
+	public void hardDelete(FuncionModel funcionModel) throws WebException {
+		Funcion funcionEntity = funcionConverter.modelToEntity(funcionModel);
+		funcionRepository.delete(funcionEntity);
+	}
+	
+	public Funcion softDelete(FuncionModel funcionModel) throws WebException {
+		Funcion funcionEntity = funcionConverter.modelToEntity(funcionModel);
+		funcionEntity.setBaja(new Date());
+		return funcionRepository.save(funcionEntity);
+	}
+	
 }
