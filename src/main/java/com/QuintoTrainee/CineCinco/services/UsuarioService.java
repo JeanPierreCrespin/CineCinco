a3e115f42f9c2304c299aff4b50766d9c2e78677
Pierre <crespinpierre98@gmail.com>
2021-04-27 19:58:15 -0300

    validar,guardar, hardDelete, sofDelete del los servicios usuario y Sala

--- src/main/java/com/QuintoTrainee/CineCinco/services/UsuarioService.java
+++ src/main/java/com/QuintoTrainee/CineCinco/services/UsuarioService.java
@@ -1,6 +1,7 @@
 package com.QuintoTrainee.CineCinco.services;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
@@ -16,7 +17,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import com.QuintoTrainee.CineCinco.converters.UsuarioConverter;
 import com.QuintoTrainee.CineCinco.entities.Usuario;
+import com.QuintoTrainee.CineCinco.exceptions.WebException;
+import com.QuintoTrainee.CineCinco.models.UsuarioModel;
 import com.QuintoTrainee.CineCinco.repositories.UsuarioRepository;
 
 @Service
@@ -24,12 +28,60 @@ public class UsuarioService implements UserDetailsService {
 	
 	@Autowired
 	private UsuarioRepository usuarioRepository;
+	@Autowired
+	private UsuarioConverter usuarioConverter;
 	
-	public void hardDelete() {}  //eliminar de la base de datos
-	public void softDelete() {}  //asignar una fecha de baja
-	public void guardar() {}     //persistir modelo
-	public void validar() {}     //valida los atributos del modelo
+	public void validar(UsuarioModel usuarioModel) throws WebException  {
+		
+		if (usuarioModel.getId() == null|| usuarioModel.getId().isEmpty() || usuarioModel.getId().equals("")) {
+			throw new WebException("El Id esta vacio");
+		}
+		if (usuarioModel.getEmail() == null|| usuarioModel.getEmail().isEmpty() || usuarioModel.getEmail().equals("")) {
+			throw new WebException("El usuario tiene que tener un Email");
+		}
+		if(usuarioModel.getNombreCompleto() ==null || usuarioModel.getNombreCompleto().isEmpty() || usuarioModel.getNombreCompleto().equals("")) {
+			throw new WebException("El usuario tiene que tener un nombre completo");
+		}
+		if(usuarioModel.getUsername() == null|| usuarioModel.getUsername().isEmpty() || usuarioModel.getUsername().equals("")) {
+			throw new WebException("El usuario tiene que tener un Nombre de Usuario");
+		}
+		if (usuarioModel.getFechaNacimiento() == null ) {
+			throw new WebException("El Usuario tiene que tener una fecha de nacimiento");
+		}
+		if(usuarioModel.getInfoTarjeta() == null || usuarioModel.getInfoTarjeta().isEmpty()) {
+			throw new WebException("El Usuario tiene que tener informacion de Tarjeta");
+		}
+		if(usuarioModel.getRol() == null) {
+			throw new WebException("El Usuario tiene que tener una Rol");
+		}
+
+	}     
 	
+	public Usuario guardar(UsuarioModel usuarioModel) throws WebException {
+		
+		validar(usuarioModel);
+		
+		Usuario usuarioEntity = usuarioConverter.modelToEntity(usuarioModel);
+		
+		if (usuarioEntity.getAlta() != null) {
+			usuarioEntity.setModificacion(new Date());
+		} else {
+			usuarioEntity.setAlta(new Date());
+		}
+		
+		return usuarioRepository.save(usuarioEntity);
+	}
+	
+	public void hardDelete(UsuarioModel usuarioModel) throws WebException {
+		Usuario usuarioEntity = usuarioConverter.modelToEntity(usuarioModel);
+		usuarioRepository.delete(usuarioEntity);
+	}
+
+	public Usuario softDelete(UsuarioModel usuarioModel) throws WebException {
+		Usuario usuarioEntity = usuarioConverter.modelToEntity(usuarioModel);
+		usuarioEntity.setBaja(new Date());
+		return usuarioRepository.save(usuarioEntity);
+	}
 	@Override
 	public UserDetails loadUserByUsername(String mail) throws UsernameNotFoundException {
 
